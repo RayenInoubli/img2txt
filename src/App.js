e377785cf9381d969dc4e752f92c4cee71d977c7
RayenInoubli <rayeninoubli29@gmail.com>
2022-10-01 13:54:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import Axios from 'axios';
+import Tesseract from 'tesseract.js';
 
 function App() {
+  const [file, setFile] = React.useState();
+  const [resText, setResText] = React.useState("");
+  const [loader, setLoader] = React.useState(false);
+
+  const convertToText = (e) => {
+    e.preventDefault();
+    
+    Tesseract.recognize(
+    file,
+    'eng',
+    { logger: m => {
+      if (m.status === "recognizing text" && m.progress !== 1) {
+        setLoader(true);
+      }else{
+        setLoader(false);
+      }
+    } }
+    ).then(({ data: { text } }) => {
+      console.log(text);
+      setResText(text);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image 2 Text</h1>
+      <form method="post" onSubmit={(e) => {convertToText(e)}}>
+        <input type="file" accept="image/JPEG image/PNG" onChange={(e) => {setFile(e.target.files[0])}}/>
+        <button type="submit">convert</button>
+        {loader && <div className='loader'></div>}
+      </form>
+      <textarea name="result" id="result" defaultValue={resText}></textarea>
     </div>
   );
 }
